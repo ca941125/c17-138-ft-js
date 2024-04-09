8b76ff9e08faaccfdc868657d55dbd780f445022
Leandro <andrianileandro@hotmail.com>
2024-04-09 20:54:07 -0300

    Update index.controller.js

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -2,9 +2,11 @@ import pool from '../db.js'
 import { db } from '@vercel/postgres'
 
 export const getIndex = async (req, res) => {
-    res.send("hola")
-    /* const data = await pool.query('SELECT * FROM alojamientos LIMIT 8 OFFSET 0')
+    /* res.send("hola") */
+    const client = await db.connect()
+    /* const data = await pool.query('SELECT * FROM alojamientos ORDER BY id DESC LIMIT 8 OFFSET 0')
     console.log(data[0])
     res.render('index', {data: data[0]}) */
-    //res.json(query)
+    const users = await client.sql`SELECT * FROM usuarios;`;
+    res.json(users)
 }
