52a3efd112c8b6c760f4dec2a36c75862f4a2b1f
Leandro <andrianileandro@hotmail.com>
2024-04-10 12:03:05 -0300

    Update index.controller.js

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -9,4 +9,5 @@ export const getIndex = async (req, res) => {
     res.render('index', {data: data[0]}) */
     const users = await client.sql`SELECT * FROM mascotas;`;
     res.json({users: users.rows})
+    res.render('index', ({users: users.rows}))
 }
