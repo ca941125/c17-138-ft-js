39974c22661152fe120e4f924fe644947e86dec8
Leandro <andrianileandro@hotmail.com>
2024-04-11 14:55:00 -0300

    Update Login

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -2,7 +2,7 @@ import pool from '../db.js'
 import { db } from '@vercel/postgres'
 
 export const getLogin = async (req, res) => {
-    const client = await db.connect()
+    /* const client = await db.connect() */
     /* res.send("hola") */
     /* const data = await pool.query('SELECT * FROM alojamientos ORDER BY id DESC LIMIT 8 OFFSET 0')
     console.log(data[0])
@@ -13,16 +13,30 @@ export const getLogin = async (req, res) => {
 }
 
 export const postLogin = async (req, res) => {
-    const { user, name, rol, pass } = req.body
+    const {user, pass} = req.body
     let passwordHaash = await bcryptjs.hash(pass, 8)
+    const client = await db.connect()
 
     try {
-        const [rows] = await pool.query(`INSERT INTO users (user, name, rol, pass) VALUES (?, ?, ?, ?)`, [user, name, rol, passwordHaash])
-        res.render('register', {
-            id: rows.insertId,
-            user
-        })
+        const data = await client.sql`SELECT * FROM usuarios WHERE nombreusuario = ${user};`;
+        console.log(data[0].pass)
+        if(data[0].length === 0 || !(await bcryptjs.compare(pass, data[0].pass))){
+            res.render('login', {msg: 'Usuario y/o contraseña incorrectas'})
+        }else{
+            req.session.loggedin = true
+            req.session.name = data[0].nombreusuario
+            req.session.idUser = data[0].usuarioid
+            req.session.rol= data[0].rol
+            res.render('login', {
+                ruta: '/admin',
+                id: data[0].usuarioid
+            })
+        }
+
     } catch (error) {
-        res.send(error)
+        res.render('login', {
+            msg: 'No se pudo iniciar sesión',
+            error
+        })
     }
 }
