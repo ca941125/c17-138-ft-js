135f7323b064c6e4f19e81eb1fe4c64948fed5ce
Leandro <andrianileandro@hotmail.com>
2024-04-11 18:36:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-import express from 'express'
-import {dirname, join} from 'path'
-import {fileURLToPath} from 'url'
-import morgan from 'morgan'
 import {PORT} from './src/config.js'
-import bcryptjs from 'bcryptjs'
-import session from 'express-session'
 import app from './src/app.js'
 
 
