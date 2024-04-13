10ce9d9f14af9ee554a2ea9f70d7857035dafc4d
Leandro <andrianileandro@hotmail.com>
2024-04-13 13:45:45 -0300

    Update index back

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -4,18 +4,34 @@ import { db } from '@vercel/postgres'
 export const getIndex = async (req, res) => {
     const client = await db.connect()
     
-    const {rows} = await client.sql`SELECT * FROM alojamientos INNER JOIN perfiles ON alojamientos.usuarioid = perfiles.usuarioid;`
-    console.log(rows)
+    const {rows} = await client.sql`SELECT perfiles.usuarioid, nombres, apellidos, direccion, ciudad, provincia, costo_alojamiento, tipo_alojamiento, aire_libre, cercado, moneda FROM alojamientos INNER JOIN perfiles ON alojamientos.usuarioid = perfiles.usuarioid;`
+    //console.log(rows[0].usuarioid)
+    /* let alojamientos = rows.map(async (data) => {
+        console.log(data.nombres)
+        const result = await client.sql`SELECT tipo_mascota FROM mascotas WHERE usuarioid = ${data.usuarioid};`
+        console.log(result.rows)
+        return data
+    }) */
+    let alojamientos = rows
+    let i = 0
+    while( i < alojamientos.length){
+        const result = await client.sql`SELECT tipo_mascota FROM mascotas WHERE usuarioid = ${alojamientos[i].usuarioid};`
+        alojamientos[i].mascotas = result.rows
+        i++
+    }
+    
+    console.log(alojamientos)
     if(req.session.loggedin){
         res.render('index', {
          login: true,
          name: req.session.name,
          id: req.session.id,
-         rol: req.session.rol
+         rol: req.session.rol,   
         })
     } else {
         res.render('index', {
-            login: false
+            login: false,
+            alojamientos
         })
     }
 
