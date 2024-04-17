02dbe6e759595d21de8e0ec76c8079e3c0bc6b09
Leandro <andrianileandro@hotmail.com>
2024-04-17 12:01:21 -0300

    Update Session

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import {fileURLToPath} from 'url'
 import morgan from 'morgan'
 import session from 'express-session'
 import cookieParser from 'cookie-parser'
+import {CLAVE} from './config.js'
 
 import indexRoutes from './routes/index.routes.js'
 import loginRoutes from './routes/login.routes.js'
@@ -13,16 +14,19 @@ import registerRoutes from './routes/register.routes.js'
 const app = express()
 const __dirname = dirname(fileURLToPath(import.meta.url))
 //console.log(__dirname)
-
+const expiryDate = new Date( Date.now() + 60 * 60 * 4000 )
 app.use(morgan('dev'))
 app.use(express.static(join(__dirname, 'public')))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 app.use(session({
-    secret: 'secret',
+    secret: CLAVE,
     resave: true,
-    saveUninitialized: 'true'
+    saveUninitialized: 'true',
+    expires: expiryDate,
+    path: 'foo/bar',
+    domain: 'https://wasi-zeta.vercel.app/'
 }))
 
 app.set('view engine', 'ejs')
