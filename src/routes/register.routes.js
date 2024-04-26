e346836f85d41e2b84f38425193cb8f6e4675ff6
Leandro <andrianileandro@hotmail.com>
2024-04-26 12:25:35 -0300

    Update axios

--- src/routes/register.routes.js
+++ src/routes/register.routes.js
@@ -1,6 +1,7 @@
 import {Router} from 'express'
 import multer from 'multer'
 import {getRegister, postRegister} from '../controllers/register.controller.js'
+import axios from 'axios'
 
 const router = Router()
 const upload = multer({ dest: 'src/public/images/upload/'} )
@@ -8,6 +9,43 @@ const upload = multer({ dest: 'src/public/images/upload/'} )
 router.get('/register', getRegister)
 
 router.post('/register', upload.any(), postRegister)
+
+router.post('/image', upload.single('image'), async (req, res) =>{
+
+// Credenciales de la aplicación Imgur
+const clientId = 'b527d48d26ca21b';
+
+// URL del endpoint para subir imágenes
+const uploadUrl = 'https://api.imgur.com/3/image';
+
+// Función para subir una imagen a Imgur
+const imageFile = req.file.toString('base64')
+console.log(imageFile)
+
+  try {
+    // Configurar la solicitud POST con la imagen y las credenciales
+    const response = await axios.post(uploadUrl, {
+      image: imageFile
+    }, {
+      headers: {
+        'Authorization': `Client-ID ${clientId}`
+      }
+    });
+    
+    // Imprimir la URL de la imagen subida
+    console.log('Image uploaded successfully:', response.data.data.link);
+    res.json({ link: response.data.data.link });
+    // Devolver la URL de la imagen subida
+    
+  } catch (error) {
+    // Manejar cualquier error que ocurra durante la carga de la imagen
+    console.error('Error uploading image:', error.message);
+    res.status(500).json({ error: 'Error al subir la imagen' });
+    
+  }
+
+
+})
 
 router.put('/register', )
 
